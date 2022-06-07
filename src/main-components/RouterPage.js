1b087021e93f105fd64691549db90953050d54a5
manojpangle05 <manojpangle05@gmail.com>
2022-06-07 11:18:54 +0530

    Update RouterPage.js

--- src/main-components/RouterPage.js
+++ src/main-components/RouterPage.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Restaurants from '../page-components/Restaurants';
 import Restaurants_2 from '../page-components/Restaurants_2';
+import Earn from '../page-components/Earn';
+import Vendor_Signin from '../page-components/Vendor_Signin';
 
 
 
@@ -17,11 +19,12 @@ export default function RouterPage() {
 
                 <Route path='/restaurants' exact element={<Restaurants/>}></Route>
                 <Route path='/restaurants-2' exact element={<Restaurants_2/>}></Route>
-                {/* <Route path='/services' exact element={<Services/>}></Route>
-                <Route path='/projects' exact element={<Projects/>}></Route> */}
+                <Route path='/earn' exact element={<Earn/>}></Route>
+                <Route path='/vendor-signin' exact element={<Vendor_Signin/>}></Route>
+                
                 </Routes>
         </Router>
      {/* <Home/> */}
     </>
-  ) 
+  )
 }
